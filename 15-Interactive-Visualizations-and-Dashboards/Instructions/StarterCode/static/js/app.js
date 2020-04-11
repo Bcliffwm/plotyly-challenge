f647ab260c31d3a1f9e758a9d99b3aad5d79f233
Ben Cliff <bocliffwm@gmail.com>
2020-04-11 14:06:51 -0400

    Added bubble chart and working demo info

--- 15-Interactive-Visualizations-and-Dashboards/Instructions/StarterCode/static/js/app.js
+++ 15-Interactive-Visualizations-and-Dashboards/Instructions/StarterCode/static/js/app.js
@@ -21,17 +21,90 @@ function unpack(rows, index) {
     buildPlot(bellyData);
 }
 
+function buildMetadata(bellyData) {
+  d3.json('samples.json').then((data)=>{
+    var metadata = data.metadata;
+    var results = metadata.filter(sampleObj => sampleObj.id == bellyData);
+    var result = results[0];
+    var panel = d3.select("#sample-metadata");
+    panel.html("");
+    Object.entries(result).forEach(([key, value])=>{
+      panel.append("h6").text(`${key.toUpperCase()}: ${value}`);
+    });
+  });
+};
+
 function buildPlot(bellyData) {
-    d3.json('samples.json').then( function(data) {
-        // var names = unpack(bellyData.dataset,0);
-        // var metadata = unpack(bellyData.dataset,1);
-        // var samples = unpack(bellyData.dataset,2);
+    d3.json('samples.json').then((data)=> {
+        var samples = data.samples;
+        var results = samples.filter(sampleObj => sampleObj.id == bellyData);
+        var result = results[0];
+        
+        var labels = result.otu_labels;
+        var ids = result.otu_ids;
+        var values = result.sample_values;
+        var sortedValues = values.sort(desc)
+
+        var bubbleLayout = {
+          title: "Cultures per Sample",
+          margin: { t: 0 },
+          hovermode: "closest",
+          xaxis: { title: "OTU ID" },
+          // margin: { t: 30}
+        };
+        var bubbleData = [{
+          x: values,
+          y: ids,
+          text: labels,
+          mode: "markers",
+          marker: {
+            size: values,
+            color: ids,
+            colorscale: "Earth", 
+          }
+        }];
+
+        var barLayout = {
+          title: "Top 10 OTUs per Sample",
+          margin: {t:0},
+          xaxis: {title: "Values"},     
+        };
+        var barData = [{
+          x: values,
+          y: ids,
+          text: labels,
+          type: "bar", 
+          orientation: 'h'
+        }];
+        Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+        Plotly.newPlot("bar", barData, barLayout);
         console.log(data)
     });
 
 }
 
+function init() {
+  var selector = d3.select("#selDataset");
+  d3.json('samples.json').then((data)=>{
+    var names = data.names;
+    names.forEach(element => {
+      selector
+        .append("option")
+        .text(element)
+        .property("value", element);
+    });
+    var firstElement = names[0];
+    buildPlot(firstElement);
+    buildMetadata(firstElement);
+  });
+}
 
+function optionChanged(newSample) {
+  buildPlot(newSample);
+  buildMetadata(newSample);
+}
+
+init();
 // Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual
 // Use sample_values as the values for the bar chart. ****
 
